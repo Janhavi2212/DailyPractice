92b2cbd4361e1a037d29c472d065ac8567f2d859
Janhavi2212 <janhavi.walde22@gmail.com>
2025-04-13 16:28:38 +0530

    remove duplicates optimized medthod

--- 11thApril/DSA/removeDuplicates.js
+++ 11thApril/DSA/removeDuplicates.js
@@ -5,7 +5,8 @@
               nums.splice(i+1,0,nums[j])
               k++
               i++;
-          }j++;
+          }
+          j++;
         }
         console.log(nums,k)
         return k
@@ -15,9 +16,25 @@
 
   var removeDuplicates1 = function(nums) {
     let   set1=new Set(nums)
-    // for(el of nums){
-    //   set1.add(el);
-    // }
     console.log([...set1])
   };
   removeDuplicates1([0,0,1,1,1,2,2,3,3,4])
+
+
+  function removeAdjacentDuplicatesCorrected(arr) {
+    if (arr.length === 0) {
+      console.log("Array is empty:", arr);
+      return;
+    }
+    let i = 0;
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[i] !== arr[j]) {
+        i++;
+        arr[i] = arr[j];
+      }
+    }
+    arr.length = i + 1; 
+    console.log("Modified array:", arr);
+  }
+  
+  removeAdjacentDuplicatesCorrected(arr);
